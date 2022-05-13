4c2156d4aff6cbc9db486ae2bcea3e5bd9d6ebf3
Jurgen Debouck <jurgen.debouck@kbc.be>
2022-05-13 10:43:22 +0200

    lab06

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -3,12 +3,9 @@ package com.ezgroceries.shoppinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.PropertySource;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableSwagger2
 public class ShoppingListApplication {
 
 	public static void main(String[] args) {
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ShoppingListConfig.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ShoppingListConfig.java
@@ -1,24 +1,11 @@
 package com.ezgroceries.shoppinglist.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 
 @Configuration
 //@PropertySource({"application.properties","application-hsqldb.properties"})
 public class ShoppingListConfig {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 
 }
